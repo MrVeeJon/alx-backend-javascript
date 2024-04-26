220fa14bb915ea47dc1513b7ded552a093f80518
Victor Jonathan <136059175+MrVeeJon@users.noreply.github.com>
2024-04-26 19:19:18 +0300

    Update 101-iterateThroughObject.js

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,3 +1,12 @@
 export default function iterateThroughObject(reportWithIterator) {
-    return reportWithIterator.join(' | ');
-  }
+    let employeeNames = [];
+
+    for (const departmentName in reportWithIterator.allEmployees) {
+        const employees = reportWithIterator.allEmployees[departmentName];
+        employeeNames.push(...employees);
+    }
+
+    return employeeNames.join('|');
+}
+
+export default iterateThroughObject;
